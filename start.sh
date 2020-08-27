58a31e1f4a25ba2b00beee8c2a653e892caa3236
Edward Akers <11798919+Winter@users.noreply.github.com>
2020-08-28 04:36:06 +1000

    Added default PUID and PGID

--- start.sh
+++ start.sh
@@ -24,7 +24,7 @@ if [ ! -z $APK_PKGS ]; then
     apk install --no-cache $APK_PKGS
 fi
 
-chown -R $PUID:$PGID "/config"
+chown -R ${PUID:-1000}:{$PGID:-1000} "/config"
 
 if [ ! -z $TZ ]; then
     echo "${TZ}" > /etc/timezone
@@ -37,4 +37,4 @@ fi
 LOGFILE="--logfile ${LOG_FILE:-/config/flexget.log}"
 LOGLEVEL="--loglevel ${LOG_LEVEL:-info}"
 
-su-exec $PUID:$PGID /config/flexget -c "config/config.yml" $LOGFILE $LOGLEVEL daemon start --autoreload-config
+su-exec ${PUID:-1000}:{$PGID:-1000} /config/flexget -c "config/config.yml" $LOGFILE $LOGLEVEL daemon start --autoreload-config
